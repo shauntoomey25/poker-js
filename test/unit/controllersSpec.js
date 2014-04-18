b147d82f63701e6960d7e41b781fdf1f587e53cb
Nathan Hibner <nhibner@gmail.com>
2014-04-18 00:20:32 -0500

    Removed excess from angular seed

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,16 +5,9 @@
 describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
-
   it('should ....', inject(function($controller) {
     //spec body
-    var myCtrl1 = $controller('MyCtrl1');
-    expect(myCtrl1).toBeDefined();
-  }));
-
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('MyCtrl2');
-    expect(myCtrl2).toBeDefined();
+    var mainCtrl = $controller('MainCtrl');
+    expect(mainCtrl).toBeDefined();
   }));
 });
